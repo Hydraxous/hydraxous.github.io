51bf7c58755cf3a411021d17d283a13dd9a5788b
Hydraxous <robo-trey@outlook.com>
2023-07-05 01:21:49 -0700

    fix modlist for mobile

--- scripts/modlist.js
+++ scripts/modlist.js
@@ -6,22 +6,25 @@ class ModEntry {
 
     getRawHtml()
     {
-        return `<div id="modListEntryTemplate">
-        <div class="container flex mx-auto justify-between bg-veryDarkBlue border-4 border-darkBlue rounded-md p-2">
-            <img src="/${this.ThumbnailURL}" alt="${this.ThumbnailAltText}" class="rounded-md border-4 border-darkBlue">
-            <div class="p-4 container flex-col mx-auto">
-                <div class=" container flex justify-between">
+        return `<div class="h-fit bg-veryDarkBlue border-4 border-darkBlue rounded-md">
+        <div class="flex flex-col md:flex-row">
+            <img src="/${this.ThumbnailURL}" alt="${this.ThumbnailAltText}" class="m-4 w-1/2 self-center md:w-1/6 rounded-md border-4 border-darkBlue">
+            <div class="flex flex-col w-full p-2 md:p-4">
+                <div class="flex flex-col md:flex-row justify-between w-full">
                     <h1 class="text-5xl text-coolWhite font-bold">${this.Name}</h1>
-                    <h2 class="text-2xl text-lapis">${this.Version}</h2>
+                    <h2 class="p-2 text-2xl text-lapis">${this.Version}</h2>
                 </div>
-                <div class="container flex justify-between pb-2">
+                <div class="flex justify-between mb-2 h-12">
                     <a href="${this.PlatformURL}"><h2 class="text-2xl text-skyBlue pt-2">${this.Platform}</h2></a>
-                    <div class="container w-1/6 flex space-x-4">
-                        <a href="${this.GithubURL}" class="text-skyBlue bg-darkBlue p-2  rounded-md hover:text-darkBlue hover:bg-skyBlue"><img src="/images/icons/githubIcon.png" alt="Github" class=""></a>
-                        <a href="${this.DownloadURL}" class="text-skyBlue bg-darkBlue border-2 border-skyBlue p-2 pl-4 pr-4 rounded-md hover:text-darkBlue hover:bg-skyBlue">DOWNLOAD</a>
+                    <div class=" flex space-x-2 text-skyBlue">
+                        <a href="${this.GithubURL}" class=" bg-darkBlue p-2 rounded-md hover:text-darkBlue hover:bg-skyBlue h-full w-full"><img src="/images/icons/githubIcon.png" alt="Github" class="h-8"></a>
+                        <a href="${this.DownloadURL}" class=" bg-darkBlue border-2 border-skyBlue align-middle p-2 rounded-md hover:text-darkBlue hover:bg-skyBlue">DOWNLOAD</a>
                     </div>
                 </div>
-                <p class="p-4 bg-darkBlue text-coolWhite rounded-md">${this.Description}</p>
+                <div>
+                    <p class="p-4 bg-darkBlue text-2xl text-coolWhite rounded-md">${this.Description}</p>
+
+                </div>
             </div>
         </div>
     </div>`;
